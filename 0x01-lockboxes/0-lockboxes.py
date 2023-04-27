b50f7ddc1bd9db983a8a83e7ec3f462b7f3c8a78
Marv <marvellousniyi123@gmail.com>
2023-04-27 06:56:15 +0100

    Guidelines followed

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,4 +1,16 @@
-def canUnlockAll(boxes):
+#!/usr/bin/python3
+def can_unlock_all(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Args:
+        boxes (List[List[int]]): A list of lists where each sublist represents
+            a box and contains integers that represent the keys that are in
+            that box.
+
+    Returns:
+        bool: True if all the boxes can be opened, False otherwise.
+    """
     n = len(boxes)
     keys = {0}
     visited = set()
@@ -9,4 +21,3 @@ def canUnlockAll(boxes):
             if key < n and key not in visited:
                 keys.add(key)
     return len(visited) == n
-
